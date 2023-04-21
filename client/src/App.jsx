f90d702ebe457604414bc9f1d0f7cd97f6a6737f
Manul Thanura <manulkarunarathna@gmail.com>
2023-04-21 18:35:46 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apikey ='xkzcxwpamp6w';
+
+const client = StreamChat.getInstance(apikey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Medi App</h1>
+    <div className='app__wrapper'>
+        <Chat client={client} theme='team light'>
+            <ChannelListContainer
+            
+            />
+            <ChannelContainer
+            />
+        </Chat>
     </div>
   )
 }
