957fdd50744410ef7f192ea77c7c36ce3f231e0e
Manul Thanura <manulkarunarathna@gmail.com>
2023-04-21 19:04:36 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'qgtk9ttyha7j';
+const apiKey = 'xkzcxwpamp6w';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
